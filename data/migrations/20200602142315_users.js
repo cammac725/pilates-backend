9e4a5755b23a23bd02218584a472cfe979648219
cammac <camm@rocktechcomputer.com>
2020-06-03 13:51:51 -0700

    added 2 exercise tables

--- data/migrations/20200602142315_users.js
+++ data/migrations/20200602142315_users.js
@@ -1,14 +1,17 @@
 
 exports.up = function(knex) {
-  return knex.schema.createTable('user', table => {
+  return knex.schema.createTable('users', table => {
     table.increments();
 
-    table.string('first_name', 255).notNullable();
-    table.string('last_name', 255).notNullable();
+    table.string('first_name', 255)
+      .notNullable()
+    table.string('last_name', 255)
+      .notNullable();
     table.string('email', 255)
       .notNullable()
       .unique()
-    table.string('password', 255).notNullable()
+    table.string('password', 255)
+      .notNullable()
   });
 };
 
---
+++ data/migrations/20200603132709_mat-exer.js
@@ -0,0 +1,24 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('mat-exers', table => {
+    table.increments();
+
+    table.string('name', 255)
+      .notNullable()
+      .unique();
+    table.string('level', 5)
+      .notNullable();
+    table.boolean('assigned');
+
+    table.integer('user_id')
+      .unsigned()
+      .references('id')
+      .inTable('users')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE')
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('mat-exers');
+};
