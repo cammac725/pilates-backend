40199848139c278db3b7b08b0a17b8dd3c0b6959
unknown <camm@rocktechcomputer.com>
2020-07-15 14:02:44 -0700

    added routes to api

--- api/server.js
+++ api/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const userRouter = require('../router/users/usersRouter');
+const matExerRouter = require('../router/exercises/matExerRouter');
+const refExerRouter = require('../router/exercises/refExerRouter');
 
 const server = express();
 
@@ -9,6 +11,8 @@ server.use(express.json());
 server.use(helmet());
 server.use(cors());
 server.use("/api/users", userRouter);
+server.use("/api/matExers", matExerRouter);
+server.use ("/api/refExers", refExerRouter);
 
 server.get('/', (req, res) => {
   res.status(200).json({ api: "pilates node api is running" })
