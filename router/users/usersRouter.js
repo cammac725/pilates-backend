f915742c51f9435d2303672f861045337aa1d864
unknown <camm@rocktechcomputer.com>
2020-08-02 21:23:25 -0700

    update put route

--- router/users/usersRouter.js
+++ router/users/usersRouter.js
@@ -26,6 +26,7 @@ router.get('/:id', (req, res) => {
 })
 
 router.put('/:id', (req, res) => {
+  const changes = req.body;
   Clients.update(req.params.id, changes)
     .then(updatedClient => {
       res.json({ updatedClient })
